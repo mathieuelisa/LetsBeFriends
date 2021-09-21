c551ee0d2fa291aac31f60d13cc1cdee8561bdd8
JulienMaurice <ju.maurice95@gmail.com>
2021-09-21 19:16:11 +0200

    🚧: Request pending

--- api/app/controllers/userController.js
+++ api/app/controllers/userController.js
@@ -8,7 +8,8 @@ const userController = {
         try {
             const result = await db.query(
                 `SELECT "user".id, "user".firstname, "user".gender, "user".email, "user".description AS bio, "user".age, "user".city, "user".phone_number AS "phoneNumber", "user".img_url AS "imgUrl", "user".created_at AS "createdAt", "user".updated_at AS "updatedAt",
-                json_agg(language.name) AS "learningLanguage",
+                json_agg(SELECT user_speak_language.language_id) AS "speakingLanguage",
+                json_agg(user_learn_language.language_id) AS "learningLanguage",
                 json_agg(
                     json_build_object(
                         'id', event.id,
@@ -30,28 +31,9 @@ const userController = {
                 JOIN "event" ON user_participate_event.event_id = event.id
                 JOIN "user_speak_language" ON user_participate_event.user_id = user_speak_language.user_id
                 JOIN "language" ON user_speak_language.language_id = language.id
+                JOIN "user_learn_language" ON user_participate_event.user_id = user_learn_language.user_id
                 WHERE "user".id = $1
                 GROUP BY "user".id`,
-                // `SELECT "user".id, "user".firstname, "user".gender, "user".email, "user".description AS bio, "user".age, "user".city, "user".phone_number AS "phoneNumber", "user".img_url AS "imgUrl", "user".created_at AS "createdAt", "user".updated_at AS "updatedAt",
-                // json_agg(json_build_object(
-                // 'id', event.id,
-                // 'title', event.title,
-                // 'description', event.description,
-                // 'startingDate', event.starting_date,
-                // 'endingDate', event.ending_date,
-                // 'imgUrl', event.img_url,
-                // 'placesLeft', event.places_left,
-                // 'longitude', event.longitude,
-                // 'latitude', event.latitude,
-                // 'ownerId', event.user_id,
-                // 'createdAt', event.created_at,
-                // 'updatedAt', event.updated_at
-                // )) AS event
-                // FROM user_participate_event 
-                // JOIN "user" ON user_participate_event.user_id = "user".id
-                // JOIN "event" ON user_participate_event.event_id = event.id
-                // WHERE "user".id = $1
-                // GROUP BY "user".id`,
                 [id], (error, result) => {
                     if (error) {
                         console.log(error)
@@ -67,6 +49,26 @@ const userController = {
     }
 
 }
+// `SELECT "user".id, "user".firstname, "user".gender, "user".email, "user".description AS bio, "user".age, "user".city, "user".phone_number AS "phoneNumber", "user".img_url AS "imgUrl", "user".created_at AS "createdAt", "user".updated_at AS "updatedAt",
+// json_agg(json_build_object(
+// 'id', event.id,
+// 'title', event.title,
+// 'description', event.description,
+// 'startingDate', event.starting_date,
+// 'endingDate', event.ending_date,
+// 'imgUrl', event.img_url,
+// 'placesLeft', event.places_left,
+// 'longitude', event.longitude,
+// 'latitude', event.latitude,
+// 'ownerId', event.user_id,
+// 'createdAt', event.created_at,
+// 'updatedAt', event.updated_at
+// )) AS event
+// FROM user_participate_event 
+// JOIN "user" ON user_participate_event.user_id = "user".id
+// JOIN "event" ON user_participate_event.event_id = event.id
+// WHERE "user".id = $1
+// GROUP BY "user".id`,
 
 
 
