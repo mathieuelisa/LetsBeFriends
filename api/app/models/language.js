40ca5384f5becc2a139fd1d53df588b7716a911d
JordanEspitalier <jordan.espitalier1@gmail.com>
2021-09-21 18:06:42 +0200

    💩🚧add sql request for findOneUserById

--- api/app/models/language.js
+++ api/app/models/language.js
@@ -1,5 +1,14 @@
-class Language {
+const CoreModel = require('./coremodel');
 
+class Language extends CoreModel {
+    static tableName = 'language'
+
+    constructor(obj) {
+		super(obj)
+		for (const propName in obj) {
+			this[propName] = obj[propName];
+		}
+	}
 };
 
 module.exports = Language;
