38d51c31cf7aff7aa1bc60b245679a8de6b086cf
mathieuelisa <mathieu.elisa@gmail.com>
2021-09-22 16:26:52 +0200

    End of contact page

--- lbf/src/Components/Home/Header/index.js
+++ lbf/src/Components/Home/Header/index.js
@@ -6,8 +6,8 @@ const Header = () => (
   <div className='header'>
     <h1 className='header__logo'>LBF</h1>
     <div className='header__navbar'>
-      <Button className='header__navbar__login' name='Login' />
-      <Button className='header__navbar__signup' name='Sign Up' />
+      <Button className='header__navbar__login' name='LOGIN' />
+      <Button className='header__navbar__signup' name='SIGN UP' />
       <Button className='header__navbar__hamburger' name='=' />
     </div>
   </div>
