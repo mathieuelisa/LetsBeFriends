18afd7f70e37d9d190641307d3769b94ef703f53
mathieuelisa <mathieu.elisa@gmail.com>
2021-09-22 09:46:52 +0200

    Adding third container to home page

---
+++ lbf/src/Components/Home/ThirdContainer/index.js
@@ -0,0 +1,12 @@
+import "./styles.scss"
+
+function ThirdContainer(){
+   return(
+      <div className="third__container">
+            <h1>Carrousel</h1>
+                <h2>footer</h2>
+      </div>
+   )
+}
+
+export default ThirdContainer
