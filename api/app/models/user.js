4b295810a70983ed4eef1ee8ba89e02499736716
JordanEspitalier <jordan.espitalier1@gmail.com>
2021-09-23 09:53:38 +0200

    update done

--- api/app/models/user.js
+++ api/app/models/user.js
@@ -129,22 +129,21 @@ class User extends CoreModel {
     async save() {
         try {
             if (this.id) {
-                await db.query(`UPDATE "user" SET firstname=$1, lastname=$2, gender=$3, email=$4, password=$5, description=$6, age=$7, city=$8, phone_number=$9, img_url=$10 WHERE id=$11`, [
-                    
-                    this.firstname,
-                    this.lastname,
-                    this.gender,
-                    this.email,
-                    this.password,
-                    this.description,
-                    this.age,
-                    this.city,
-                    this.phone_number,
-                    this.img_url,
-                    this.id
-                ])
+                let count = 1;
+                const properties = [];
+                const values = [this.id];
 
-                return this
+                for (const key in this){
+                    if(key == 'id') continue;
+                    properties.push(`"${key}"=$${++count}`)
+                    values.push(this[key])
+                }
+    
+                console.log(properties)
+                console.log(`UPDATE "user" SET ${properties} WHERE id=$1 RETURNING *`)
+                console.log(values)
+                const { rows } = await db.query(`UPDATE "user" SET ${properties} WHERE id=$1 RETURNING *`, values)
+                return new User(rows[0])
             } else {
                 const { rows } = await db.query('INSERT INTO "user"(firstname, lastname, gender, email, password, description, age, city, phone_number, img_url) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id', [
                     this.firstname,
