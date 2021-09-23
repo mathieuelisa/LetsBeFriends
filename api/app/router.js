2d5fa92c16a88906c37fe47f1d07cb2189d84662
emmanuelMartin53 <https://github.com/emmanuelMartin53/emmanuelMartin53.git>
2021-09-23 10:17:27 +0200

    jsdoc router

--- api/app/router.js
+++ api/app/router.js
@@ -8,15 +8,51 @@ const tagController = require('./controllers/tagController');
 const userController = require('./controllers/userController');
 
 // EVENT
+
+// GET/events/:id
+/**
+ * Respond with one event from database
+ * @route GET / events/{id}
+ * @group Event
+ * @summary Responds with event from database
+ * @param {number} id.path.required The id of the event to fetch
+ * @returns {Event.model} 200 - A single post identified by its id
+ * @returns {string} 404 - An error message
+ * @returns {string} 500 - An error message
+ */
 router.get('/events/:id', eventController.findOneById)
 
 // LANGUAGE
 
 // TAG
-ss
+
 // USER
 
-router.get('/users/:id', userController.findOneById)
+// GET / users
+/**
+ * Responds with all users in database
+ * @route GET/users
+ * @group User
+ * @summary Responds with all users in database
+ * @returns {Array<User>} 200 - An array of users
+ * @returns {string} 500 - An error message 
+ */
 router.get('/users', userController.findAll)
+
+// GET /users/:id
+
+/**
+ * Respond with one user from database
+ * @route GET / users/{id}
+ * @group User
+ * @summary Responds with one user from database
+ * @param {number} id.path.required The id of the user to fetch
+ * @returns {User.model} 200 - A single post identified by its id
+ * @returns {string} 404 - An error message
+ * @returns {string} 500 - An error message
+ */
+
+router.get('/users/:id', userController.findOneById)
+
 
 module.exports = router;
