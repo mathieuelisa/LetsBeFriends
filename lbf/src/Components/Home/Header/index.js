7a8e109610e931bb821a5a1580998aa1d0ab0fbf
Marob-nod <robin.marien@hotmail.fr>
2021-09-25 14:43:10 +0200

    🚧 - Toggle menu button working with settings changing className - Need to adapt CSS

--- lbf/src/Components/Home/Header/index.js
+++ lbf/src/Components/Home/Header/index.js
@@ -11,7 +11,7 @@ import { useDispatch, useSelector } from "react-redux"
 const Header = ({ openModalLogin, openModalSignup }) => {
 
 const dispatch = useDispatch()
-const toggleAction = useSelector((state)=> state.toggleAction)
+const toggleAction = useSelector((state)=> state.common.toggleAction)
 
   function handleClick(event){
     event.preventDefault()
@@ -25,16 +25,14 @@ const toggleAction = useSelector((state)=> state.toggleAction)
       <div className='header__navbar'>
         <ButtonModal openModal={openModalLogin} className='header__navbar__login' name='LOGIN' />
         <ButtonModal openModal={openModalSignup} className='header__navbar__signup' name='SIGN UP' />
-        {/* <div className={`settings__container${toggleAction ? ' open' : "" }`}> */}
-        {/* <Button className='header__navbar__hamburger' name='='  /> */}
-          {/* <ButtonToggle className='header__navbar__hamburger' classNameDiv='searchEvent-button-menuhome' name='=' handleClick={handleClick}/> */}
+        <div className={toggleAction ? 'settings__container' : 'settings__container.open'}>
           <ButtonToggle 
             className='settings__container--toggle' 
-            classNameDiv={`settings__container${toggleAction ? ' open' : "" }`} 
+            // classNameDiv={`settings__container${toggleAction ? ' open' : "" }`} 
             name='=' 
             handleClick={handleClick}
           />
-        {/* </div> */}
+        </div>
       </div>
     </div>
   );
