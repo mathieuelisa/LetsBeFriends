72547cf185664bcbc4a412e4861ccce4de22b9a7
JordanEspitalier <jordan.espitalier1@gmail.com>
2021-09-27 15:09:24 +0200

    update heroku on developpment branch

--- api/app/router.js
+++ api/app/router.js
@@ -1,4 +1,4 @@
-const express = require('express');
+ const express = require('express');
 const router = express.Router();
 
 const { eventController, languageController, tagController, userController } = require('./controllers/index')
