f07ee266060692f5da15504170a579366b5955ec
JordanEspitalier <jordan.espitalier1@gmail.com>
2021-09-27 13:03:38 +0200

    adaptation db distante

--- api/app/database.js
+++ api/app/database.js
@@ -1,8 +1,14 @@
 const {Pool} = require('pg');
-
-const pool = new Pool({
+const config = {
     connectionString: process.env.DATABASE_URL
-});
+}
+if(process.env.NODE_ENV === 'production'){
+    // config pour la version de prod heroku
+    config.ssl = {
+        rejectUnauthorized : false
+    }
+}
+const pool = new Pool(config);
 
 pool.connect();
 
