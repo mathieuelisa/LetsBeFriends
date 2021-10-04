e70c2ce9e8ecd46188d24b5329fd431db5871cfd
mathieuelisa <mathieu.elisa@gmail.com>
2021-10-04 11:58:03 +0200

    Test

--- lbf/src/Components/Styledcomponents/Loader/index.js
+++ lbf/src/Components/Styledcomponents/Loader/index.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-const Loader = () => {
-    return (
-
-        <svg version="1.1" id="L3" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-            viewBox="0 0 100 100" enable-background="new 0 0 0 0" xmlSpace="preserve">
-            <circle fill="none" stroke="#fff" stroke-width="4" cx="50" cy="50" r="44" style="opacity:0.5;" />
-            <circle fill="#fff" stroke="#e74c3c" stroke-width="3" cx="8" cy="54" r="6" >
-                <animateTransform
-                    attributeName="transform"
-                    dur="2s"
-                    type="rotate"
-                    from="0 50 48"
-                    to="360 50 52"
-                    repeatCount="indefinite" />
-
-=======
 //Import styles
 import "./styles.scss"
 //commentaire test
@@ -60,7 +43,6 @@ const Loader = ({ size = 50, alternative = false }) => {
                 dur="0.9s"
                 repeatCount="indefinite"
               />
->>>>>>> reduxInProgress
             </circle>
         </svg>
 
