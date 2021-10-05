da287378a54a807e73908c94ee865041d3fec9a0
mathieuelisa <mathieu.elisa@gmail.com>
2021-10-05 12:45:01 +0200

    Adding pics on homePage

--- lbf/src/Components/Home/MainContainer/index.js
+++ lbf/src/Components/Home/MainContainer/index.js
@@ -6,6 +6,9 @@ import "./styles.scss";
 import "../../../assets/Fonts/Surfing2.woff";
 //Import pictures
 import wallpaper from "../../../assets/Wallpaper/wallpaper.jpg";
+import title from "../../../assets/Img/title.png"
+import subtitle from "../../../assets/Img/subtitle.png"
+import titlePhone from "../../../assets/Img/titlePhone.png"
 //React Components
 import Modal from "../../Styledcomponents/Modal";
 import Header from "../Header";
@@ -44,10 +47,14 @@ function MainContainer() {
         />
         <Modal openModale={openModale} />
         <div className="main__container-text">
-          <h3 className="main__container--secondTitle">
-            The simpliest way to meet people and train languages !
-          </h3>
-          <h1 className="main__container--title">LETS BE FRIENDS</h1>
+          <div>
+            <img className="main__container-image1" src={subtitle} alt="logo" />
+          </div>
+
+          <div className="main__container-divTitle">
+            <img className="main__container-image2" src={title} alt="logo" />
+            <img className="main__container-image3" src={titlePhone} alt="logo" />
+          </div>
         </div>
       </div>
     </div>
