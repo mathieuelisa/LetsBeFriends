b4f34a326dfeb379386001cc5a63c3bb879f69c1
mathieuelisa <mathieu.elisa@gmail.com>
2021-10-09 15:44:41 +0200

    Adding custom error 404 page

--- lbf/src/Components/Error404/MainContainer/index.js
+++ lbf/src/Components/Error404/MainContainer/index.js
@@ -1,12 +1,24 @@
 // Import styles
 import "./styles.scss"
 import "../../../"
+// Import Gif
+import LogoError from "../../../assets/Logo/chevre.gif"
+import { Link } from "react-router-dom"
 
 function ErrorContainer(){
     return(
         <div className="errorContainer__container" >
-            <h1>ERROR 404</h1>
-            <h2>Sorry, the page you requested was not found</h2>
+            <div className="errorContainer__container-containerPictures">
+                <img className="errorContainer__container-containerPictures-pictures" src={LogoError} alt="Logo Error 404" />
+            </div>
+
+            <div className="errorContainer__container-containerText">
+                <h1>OOPS !</h1>
+                <h2 className="errorContainer__container-containerText-second">Someone seems to have eaten the page you were looking for...</h2>
+            </div>
+
+           <Link to="/"> <button className="errorContainer__container-errorButton">Home page ? </button></Link>
+           
         </div>
     )
 }
